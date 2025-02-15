ad3b7dd8f7b6ecd379b7fdf43f46ebb22eabf53c
Mohit Bhalotia <mohitkumarbhalotia420@gmail.com>
2025-02-15 10:47:00 +0530

    Added admin

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -1,13 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "font-awesome/css/font-awesome.min.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
+import ScrollToTop from "./pages/scrollToTop.jsx";
 import store from "./store/store.js";
+import { ToastContainer } from "react-toastify";
 import routes from "./routes/Routes.jsx";
-import ScrollToTop from "./pages/ScrollToTop.jsx";
-import { ToastContainer } from 'react-toastify';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,7 +14,7 @@ const router = createBrowserRouter([
       <>
         <ScrollToTop />
         <App />
-        <ToastContainer position="top-center"/>
+        <ToastContainer position="top-center" />
       </>
     ),
     children: routes,
